495ae2d22cad0824a5e167878afe35e9ee60c3da
ilyasUgur <83614000+ilyasUgur@users.noreply.github.com>
2022-10-26 00:32:41 +0200

    Oktober

---
+++ target/cucumber-report/report.js
@@ -0,0 +1,96 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("src/test/java/featureFiles/HumanResources.feature");
+formatter.feature({
+  "line": 1,
+  "name": "PositionSalary",
+  "description": "",
+  "id": "positionsalary",
+  "keyword": "Feature"
+});
+formatter.background({
+  "line": 3,
+  "name": "",
+  "description": "",
+  "type": "background",
+  "keyword": "Background"
+});
+formatter.step({
+  "line": 4,
+  "name": "Navigate to Basqar",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "Enter valid username and password",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "Success message should be displayed",
+  "keyword": "Then "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.scenario({
+  "line": 8,
+  "name": "Navigate to Human Resources",
+  "description": "",
+  "id": "positionsalary;navigate-to-human-resources",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 9,
+  "name": "Click to Human Resources",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "Click to Positions",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "Add position and Name and ShortName",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "Save and Delete to Positions",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "Then Success message should be displayed",
+  "keyword": "And "
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+formatter.match({});
+formatter.result({
+  "status": "undefined"
+});
+});
